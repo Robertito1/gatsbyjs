72d538e7ad51f034740d0e5805d449a835d84d85
robertito1 <orazurobert@yahoo.com>
2021-03-06 22:28:56 +0000

    projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -4,12 +4,42 @@ import { StaticImage } from "gatsby-plugin-image"
 const Projects = () => {
     return ( 
         <div  style={{ backgroundColor: '#262626'}} className='rounded-b-md text-white mb-8 px-8 py-20 md:px-20 lg:px-30 '>
-            <p>Some of my works</p>
-            <div className='w-full flex flex-col justify-around align-center lg:flex-row justify-between '>
-                <div>
-                <StaticImage src="../images/dorcas.png" width={500} quality={95} alt="A dinosaur" className='p-0' /> 
+            <p className='text-3xl mb-4'>Some of my works</p>
+            <div className='w-full mb-40 flex flex-col justify-around items-center align-center lg:flex-row justify-between '>
+                <div className='my-auto px-4'>
+                <StaticImage src="../images/dorcas.png" width={800} quality={95} alt="A dinosaur" className='p-0' /> 
                 </div>
-                <div>Write up</div>
+                <div className='px-4 w-full lg:w-1/4'>
+               <p className='text-xl'> Type:</p>
+               <p className='text-xl mb-4'>A portfolio Site</p>
+               <p className='text-xl'>Tech Stack:</p>
+               <ol className='mb-4'>
+                 <li className='text-xl'>-HTML</li>
+                 <li className='text-xl'>-CSS</li>
+                 <li className='text-xl'>-JavaScript</li>
+                 <li className='text-xl'>-BootStrap</li>
+               </ol>
+               <p className='text-xl'> Description:</p>
+               <p className='text-xl'>A portfolio Site for a Human Resource Consultant</p>
+                </div>
+           </div>
+           <div className='w-full  mb-6 flex flex-col justify-around items-center align-center lg:flex-row justify-between '>
+                <div className='my-auto px-4 order-1 lg:order-2'>
+                <StaticImage src="../images/beauty.png" width={800} quality={95} alt="A dinosaur" className='p-0' /> 
+                </div>
+                <div className='px-4 order-2 w-full lg:w-1/4 lg:order-1'>
+               <p className='text-xl'> Type:</p>
+               <p className='text-xl mb-4'>A Landing Page</p>
+               <p className='text-xl'>Tech Stack:</p>
+               <ol className='mb-4'>
+                 <li className='text-xl'>-HTML</li>
+                 <li className='text-xl'>-CSS</li>
+                 <li className='text-xl'>-JavaScript</li>
+                 <li className='text-xl'>-BootStrap</li>
+               </ol>
+               <p className='text-xl'> Description:</p>
+               <p className='text-xl'>The landing page of a beauty and cosmetics E-commerce shop</p>
+                </div>
            </div>
         </div>
      );
