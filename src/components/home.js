bfc70715324bceec148e00dbd878827be9fba56a
robertito1 <orazurobert@yahoo.com>
2021-03-15 13:33:40 +0100

    animation done

--- src/components/home.js
+++ src/components/home.js
@@ -1,21 +1,20 @@
 import React from "react";
-// import ReactAnime from 'react-animejs'
 import { StaticImage } from "gatsby-plugin-image"
-// const {Anime} = ReactAnime
 
 function Main() {
   return (
    <div style={{ backgroundColor: '#262626'}} className='w-full rounded-t-md flex flex-col justify-around align-center px-8 py-20 md:px-20 md:py-28 lg:flex-row lg:justify-between lg:px-30 lg:py-40'>
-    <div className='w-full order-2 lg:order-1 flex flex-col text-white justify-center align-center items-center lg:items-start lg:pr-8'>
+    <div className='w-full lg:w-1/2 order-2 lg:order-1 flex flex-col text-white justify-center align-center items-center lg:items-start lg:pr-8'>
       <h2 className='text-8xl fancytext aTitleText'>HELLO</h2>
       <p className='text-xl'>I’m Robert Orazu, a Software Developer from Lagos Nigeria,  
       I specialize in efficient React and Node.js apps that just work across all platforms and browsers. 
-      I care about building solutions that are usable and pleasant for the most number of people possible. </p>
+      I care about building solutions that are usable and pleasant for the most number of people possible. I am interested in various
+      technologies and I am always eager to learn new things</p>
       <p className='text-xl'>You can find me on other platforms using the corresponding icon at the footer of this website.</p>
       <p className='text-2xl text-yellow-200  mt-4 aTitleText'>CHEERS ROBERT</p>
     </div>
     <div className='flex order-1 lg:order-2 justify-center mt-12 lg:mt-0 lg:justify-start'>
-      <StaticImage src="../images/robert.jpg" width={300} quality={95} alt="A dinosaur" className='p-0' /> 
+      <StaticImage src="../images/robert.jpg" width={300} quality={95} alt="A dinosaur" className='p-0 rounded-full' /> 
     </div>
    </div>
   )
--- src/components/skills.js
+++ src/components/skills.js
@@ -1,15 +1,266 @@
 import React from 'react';
-import '../styles/skills.css'
+import '../styles/skills.css';
+import ReactAnime from 'react-animejs'
+
 
 const Skills = () => {
-    
+ const {Anime} = ReactAnime;
+  
     return ( 
         <div id='skills'style={{ backgroundColor: '#262626'}} className='text-white px-8 py-20 md:px-20 lg:px-30'>
-            {/* <div>
-                <h4 className='text-white text-4xl express'>
-                    EXPRESS
-                </h4>
-            </div> */}
+                <Anime
+                    className='hidden lg:inline'
+                    initial={[
+                        {
+                        targets: "#express",
+                        keyframes: [
+                            {translateY: -10},
+                            {translateX: 20},
+                            {translateY: 0},
+                            {color: '#68f7d5', duration: 10},
+                            {color: '#262626'},
+                            {translateX: 0},
+                          ],
+                        duration: 3000,
+                        easing: "linear",
+                        loop: true
+                        }
+                    ]}
+                    >
+                    <h4 style={{color: '#262626'}} id="express" className='text-white text-4xl font-bold express'>
+                      EXPRESS
+                    </h4>
+                </Anime>
+                <Anime
+                    className='hidden lg:inline'
+                    initial={[
+                        {
+                        targets: "#react",
+                        keyframes: [
+                            {translateY: -20},
+                            {translateX: 10},
+                            {translateY: 0},
+                            {color: '#68f7d5', duration: 10},
+                            {color: '#262626'},
+                            {translateX: 0},
+                          ],
+                        duration: 3000,
+                        easing: "linear",
+                        loop: true
+                        }
+                    ]}
+                    >
+                    <h4 style={{color: '#262626'}} id="react" className='text-white text-7xl font-extrabold react'>
+                      REACT.JS
+                    </h4>
+                </Anime>
+                <Anime
+                    className='hidden lg:inline'
+                    initial={[
+                        {
+                        targets: "#node",
+                        keyframes: [
+                            {translateY: -15},
+                            {translateX: 15},
+                            {translateY: 0},
+                            {color: '#68f7d5', duration: 10},
+                            {color: '#262626'},
+                            {translateX: 0},
+                          ],
+                        duration: 4000,
+                        easing: "linear",
+                        loop: true
+                        }
+                    ]}
+                    >
+                    <h4 style={{color: '#262626'}} id="node" className='text-white text-6xl font-extrabold node'>
+                      NODE.JS
+                    </h4>
+                </Anime>
+                <Anime
+                    className='hidden lg:inline'
+                    initial={[
+                        {
+                        targets: "#jest",
+                        keyframes: [
+                            {translateY: 15},
+                            {translateX: -15},
+                            {translateY: 0},
+                            {color: '#68f7d5', duration: 10},
+                            {color: '#262626'},
+                            {translateX: 0},
+                          ],
+                        duration: 3300,
+                        easing: "linear",
+                        loop: true
+                        }
+                    ]}
+                    >
+                    <h4 style={{color: '#262626'}} id="jest" className='text-white text-3xl font-extrabold jest'>
+                      JEST
+                    </h4>
+                </Anime>
+                <Anime
+                    className='hidden lg:inline'
+                    initial={[
+                        {
+                        targets: "#mongo",
+                        keyframes: [
+                            {translateY: 35},
+                            {translateX: -15},
+                            {translateY: 0},
+                            {color: '#68f7d5', duration: 10},
+                            {color: '#262626'},
+                            {translateX: 0},
+                          ],
+                        duration: 4300,
+                        easing: "linear",
+                        loop: true
+                        }
+                    ]}
+                    >
+                    <h4 style={{color: '#262626'}} id="mongo" className='text-white text-6xl font-extrabold mongo'>
+                      MONGO DB
+                    </h4>
+                </Anime>
+                <Anime
+                    className='hidden lg:inline'
+                    initial={[
+                        {
+                        targets: "#tailwind",
+                        keyframes: [
+                            {translateY: 15},
+                            {translateX: -15},
+                            {translateY: 0},
+                            {color: '#68f7d5', duration: 10},
+                            {color: '#262626'},
+                            {translateX: 0},
+                          ],
+                        duration: 3000,
+                        easing: "linear",
+                        loop: true
+                        }
+                    ]}
+                    >
+                    <h4 style={{color: '#262626'}} id="tailwind" className='text-white text-4xl font-extrabold tailwind'>
+                      TAILWIND CSS
+                    </h4>
+                </Anime>
+                <Anime
+                    className='hidden lg:inline'
+                    initial={[
+                        {
+                        targets: "#typescript",
+                        keyframes: [
+                            {translateY: 25},
+                            {translateX: 15},
+                            {translateY: 0},
+                            {color: '#68f7d5', duration: 10},
+                            {color: '#262626'},
+                            {translateX: 0},
+                          ],
+                        duration: 4000,
+                        easing: "linear",
+                        loop: true
+                        }
+                    ]}
+                    >
+                    <h4 style={{color: '#262626'}} id="typescript" className='text-white text-4xl font-extrabold typescript'>
+                      TYPESCRIPT
+                    </h4>
+                </Anime>
+                <Anime
+                    className='hidden lg:inline'
+                    initial={[
+                        {
+                        targets: "#ci",
+                        keyframes: [
+                            {translateY: -10},
+                            {translateX: 10},
+                            {translateY: 0},
+                            {color: '#68f7d5', duration: 10},
+                            {color: '#262626'},
+                            {translateX: 0},
+                          ],
+                        duration: 3000,
+                        easing: "linear",
+                        loop: true
+                        }
+                    ]}
+                    >
+                    <h4 style={{color: '#262626'}} id="ci" className='text-white text-3xl font-bold ci'>
+                      CI/CD
+                    </h4>
+                </Anime>
+                <Anime
+                    className='hidden lg:inline'
+                    initial={[
+                        {
+                        targets: "#graphql",
+                        keyframes: [
+                            {translateY: -20},
+                            {translateX: 10},
+                            {translateY: 0},
+                            {color: '#68f7d5', duration: 10},
+                            {color: '#262626'},
+                            {translateX: 0},
+                          ],
+                        duration: 5000,
+                        easing: "linear",
+                        loop: true
+                        }
+                    ]}
+                    >
+                    <h4 style={{color: '#262626'}} id="graphql" className='text-3xl font-bold graphql'>
+                      GRAPHQL
+                    </h4>
+                </Anime>
+                <Anime
+                    className='hidden lg:inline'
+                    initial={[
+                        {
+                        targets: "#redux",
+                        keyframes: [
+                            {translateY: 20},
+                            {translateX: 10},
+                            {translateY: 0},
+                            {color: '#68f7d5', duration: 10},
+                            {color: '#262626'},
+                            {translateX: 0},
+                          ],
+                        duration: 5000,
+                        easing: "linear",
+                        loop: true
+                        }
+                    ]}
+                    >
+                    <h4 style={{color: '#262626'}} id="redux" className='text-3xl font-bold redux'>
+                      REDUX
+                    </h4>
+                </Anime>
+                <Anime
+                    className='hidden lg:inline'
+                    initial={[
+                        {
+                        targets: "#javascript",
+                        keyframes: [
+                            {translateY: 20},
+                            {translateX: 10},
+                            {translateY: 0},
+                            {color: '#68f7d5', duration: 10},
+                            {color: '#262626'},
+                            {translateX: 0},
+                          ],
+                        duration: 6000,
+                        easing: "linear",
+                        loop: true
+                        }
+                    ]}
+                    >
+                    <h4 style={{color: '#262626'}} id="javascript" className='text-5xl font-extrabold javascript'>
+                      JAVASCRIPT
+                    </h4>
+                </Anime>
         <h4 className='mb-8 lg:mb-20 text-4xl md:text-6xl font-bold sectiontext aTitleText'>Industry relevant Skills</h4>
         <div>
         <p className='text-2xl'>Programming Languages</p>
