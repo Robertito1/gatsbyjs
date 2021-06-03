b4770b842c5a60eab16d243b09d75efb227779a8
Robert Orazu <orazurobert@yahoo.com>
2021-06-03 11:51:34 +0100

    Update profileCard.js

--- src/components/profileCard.js
+++ src/components/profileCard.js
@@ -4,7 +4,7 @@ import { StaticImage } from "gatsby-plugin-image"
 const ProfileCard =() => {
     return (
         <div className='w-full flex px-8 py-10 md:px-20 md:py-10 lg:align-center lg:flex-row lg:justify-left lg:py-10 '>
-               <StaticImage src="../../images/robert_image.jpg" width={80} quality={95} alt="A dinosaur"
+               <StaticImage src="../images/robert_image.jpg" width={80} quality={95} alt="A dinosaur"
                className='p-0 rounded-full m-4 ml-0' />
                <div className='flex align-center flex-col justify-center'>
                  <p className='text-white text-xl font-semibold md:text-2xl lg:text-3xl'>ROBERT ORAZU</p>
