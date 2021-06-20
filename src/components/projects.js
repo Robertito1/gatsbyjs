7fb24677c86fc3277682bb6a8cb3f5ce2b4f6ef6
Robert <orazurobert@yahoo.com>
2021-06-20 11:02:02 +0100

    added guala project

--- src/components/projects.js
+++ src/components/projects.js
@@ -16,20 +16,20 @@ const Projects = () => {
             <h1 className='text-4xl md:text-6xl font-bold mb-8 lg:mb-20 sectiontext aTitleText'>Some of my works</h1>
             <div className='w-full mb-40 flex flex-col justify-between items-center align-center lg:flex-row justify-between'  data-aos="fade-up">
                 <div className='my-auto lg:pr-4'>
-                <StaticImage src="../images/dorcas.png" width={800} quality={95} alt="A dinosaur" className='p-0' /> 
+                <StaticImage src="../images/guala.png" width={800} quality={95} alt="A dinosaur" className='p-0' /> 
                 </div>
                 <div className='w-full pl-0 lg:w-1/4 lg:pl-4'>
                <h1 className='text-xl aTitleText'> Type:</h1>
-               <p className='text-xl mb-4'>A portfolio Site</p>
+               <p className='text-xl mb-4'>A Fintech App</p>
                <h1 className='text-xl aTitleText'>Tech Stack:</h1>
                <ol className='mb-4'>
-                 <li className='text-xl'><p>-HTML</p></li>
-                 <li className='text-xl'><p>-CSS</p></li>
-                 <li className='text-xl'><p>-JavaScript</p></li>
-                 <li className='text-xl'><p>-BootStrap</p></li>
+                 <li className='text-xl'><p>-React.js</p></li>
+                 <li className='text-xl'><p>-TailwindCSS</p></li>
+                 <li className='text-xl'><p>-Formik</p></li>
+                 <li className='text-xl'><p>-Yup</p></li>
                </ol>
                <h1 className='text-xl aTitleText'>Description:</h1>
-               <p className='text-xl mb-4'>A portfolio Site for a Human Resource Consultant</p>
+               <p className='text-xl mb-4'>Fintech App used to manage business also personal assests</p>
                <a href='https://robertito1.github.io/project23/' role='button' className='viewProjectBtn text-black font-bold'>VIEW PROJECT</a>
                 </div>
            </div>
@@ -50,6 +50,25 @@ const Projects = () => {
                <h1 className='text-xl'> Description:</h1>
                <p className='text-xl mb-4'>The landing page of a beauty and cosmetics E-commerce shop</p>
                <a href='https://robertito1.github.io/beauty/' role='button' className='viewProjectBtn text-black font-bold'>VIEW PROJECT</a>
+                </div>
+           </div>
+           <div className='w-full mb-40 flex flex-col justify-between items-center align-center lg:flex-row justify-between'  data-aos="fade-up">
+                <div className='my-auto lg:pr-4'>
+                <StaticImage src="../images/dorcas.png" width={800} quality={95} alt="A dinosaur" className='p-0' /> 
+                </div>
+                <div className='w-full pl-0 lg:w-1/4 lg:pl-4'>
+               <h1 className='text-xl aTitleText'> Type:</h1>
+               <p className='text-xl mb-4'>A portfolio Site</p>
+               <h1 className='text-xl aTitleText'>Tech Stack:</h1>
+               <ol className='mb-4'>
+                 <li className='text-xl'><p>-HTML</p></li>
+                 <li className='text-xl'><p>-CSS</p></li>
+                 <li className='text-xl'><p>-JavaScript</p></li>
+                 <li className='text-xl'><p>-BootStrap</p></li>
+               </ol>
+               <h1 className='text-xl aTitleText'>Description:</h1>
+               <p className='text-xl mb-4'>A portfolio Site for a Human Resource Consultant</p>
+               <a href='https://robertito1.github.io/project23/' role='button' className='viewProjectBtn text-black font-bold'>VIEW PROJECT</a>
                 </div>
            </div>
         </div>
